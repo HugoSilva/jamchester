69acf8e592b6421fe43bd9300d9ce143c1c88521
Hugo Silva <hugoflsilva@gmail.com>
2017-06-24 12:14:51 +0100

    Added multiple platform elements in the test scene

---
+++ Assets/Scripts/Platform.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour {
+
+    public Transform platform;
+    public Transform begin;
+    public Transform end;
+    private bool startDirection;
+    public float speed = 1.0F;
+    private float startTime;
+    private float journeyLength;
+
+    void Awake () {
+        platform.position = begin.position;
+        startDirection = true;
+        startTime = Time.time;
+        journeyLength = Vector3.Distance(begin.position, end.position);
+    }
+
+	void Update () {
+
+        Vector3 current = platform.position;
+        float distCovered = (Time.time - startTime) * speed;
+        float fracJourney = distCovered / journeyLength;
+        if (startDirection) {
+            platform.position = Vector3.Lerp(begin.position, end.position, fracJourney);
+            if(Vector3.Distance(platform.position, end.position) == 0) {
+                startDirection = false;
+                startTime = Time.time;
+            }
+        }
+        else {
+            platform.position = Vector3.Lerp(end.position, begin.position, fracJourney);
+            if (Vector3.Distance(platform.position, begin.position) == 0) {
+                startDirection = true;
+                startTime = Time.time;
+            }
+        }
+    }
+}
