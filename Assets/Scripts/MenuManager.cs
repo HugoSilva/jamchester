98b7d823b1e362294c37387ac678af685a653d78
Hugo Silva <hugoflsilva@gmail.com>
2017-06-25 07:53:09 +0100

    Changes to the menu and new sound effects

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,9 +1,20 @@
 ﻿using UnityEngine.SceneManagement;
 using UnityEngine;
 
+struct Hamsters {
+    GameObject model;
+    Sprite label;
+    AudioSource sound;
+}
+
 public class MenuManager : MonoBehaviour {
     bool start = false;
-    bool playersSelected = false;
+    bool player1Selected = false;
+    public GameObject selectPlayer1;
+    public GameObject selectPlayer2;
+    public GameObject selectedPlayer1;
+    public GameObject selectedPlayer2;
+    bool player2Selected = false;
     public GameObject startText;
     public GameObject characterSelect;
     public AudioSource startSound;
@@ -14,8 +25,12 @@ public class MenuManager : MonoBehaviour {
         cInput.SetKey("left1", Keys.Xbox1DPadLeft);
         cInput.SetKey("right1", Keys.Xbox1DPadRight);
         cInput.SetKey("select1", Keys.Xbox1A);
-        //cInput.SetKey("start1", Keys.Xbox1Start);
-        cInput.SetKey("start1", Keys.M);
+        cInput.SetKey("start1", Keys.Xbox1Start);
+        //TEST ONLY
+        //cInput.SetKey("left1", Keys.A);
+        //cInput.SetKey("right1", Keys.D);
+        //cInput.SetKey("select1", Keys.E);
+        //cInput.SetKey("start1", Keys.Q);
 
         cInput.SetKey("left2", Keys.Xbox2DPadLeft);
         cInput.SetKey("right2", Keys.Xbox2DPadRight);
@@ -32,11 +47,19 @@ public class MenuManager : MonoBehaviour {
             startText.SetActive(false);
             characterSelect.SetActive(true);
             if (cInput.GetButtonDown("select1")) {
-                playersSelected = true;
+                player1Selected = true;
+                selectPlayer1.SetActive(false);
+                selectedPlayer1.SetActive(true);
+                PlaySelectedSFX();
+            }
+            if (cInput.GetButtonDown("select2")) {
+                player2Selected = true;
+                selectPlayer2.SetActive(false);
+                selectedPlayer2.SetActive(true);
+                PlaySelectedSFX();
             }
         }
-        if (playersSelected) {
-            PlayButtonSFX();
+        if (player1Selected && player2Selected) {
             StartGame();
         }
     }
@@ -45,7 +68,11 @@ public class MenuManager : MonoBehaviour {
         startSound.Play();
     }
 
+    void PlaySelectedSFX() {
+        selectSound.Play();
+    }
+
     public void StartGame() {
-        SceneManager.LoadScene("Game");
+        SceneManager.LoadScene("LukeTestScene");
     }
 }
